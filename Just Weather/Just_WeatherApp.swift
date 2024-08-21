0e6b1ad9d7ea889e150788fb148324e9e13d0e57
Sam Clemente <sam@theclementes.com>
2024-08-21 19:38:50 -0400

    Initial Commit

---
+++ Just Weather/Just_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_WeatherApp.swift
+//  Just Weather
+//
+//  Created by Sam Clemente on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Just_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Just WeatherTests/Just_WeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  Just_WeatherTests.swift
+//  Just WeatherTests
+//
+//  Created by Sam Clemente on 8/21/24.
+//
+
+import Testing
+@testable import Just_Weather
+
+struct Just_WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
